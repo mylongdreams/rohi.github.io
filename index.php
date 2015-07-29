44585ec398b140702659b1a9f5af93d513a327d9
rohit kumar <kumar.rohit1308@gmail.com>
2015-07-29 16:51:09 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,445 @@
+	<!DOCTYPE html>
+	<html lang="en">
+	  <head>
+		<!-- Basic Page Needs
+		================================================== -->
+		<meta charset="utf-8">
+		<!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Review your colleges here !!</title>
+		
+		<!-- Favicons
+		================================================== -->
+		<link rel="shortcut icon" href="img/logo2.jpg" type="image/x-icon">
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+		<!-- Bootstrap -->
+	<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+		<!-- Slider
+		================================================== -->
+		<link href="css/owl.carousel.css" rel="stylesheet" media="screen">
+		<link href="css/owl.theme.css" rel="stylesheet" media="screen">
+
+		<!-- Stylesheet
+		================================================== -->
+	   <link rel="stylesheet" type="text/css"  href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/responsive.css">
+		
+		<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,700,300,600,800,400' rel='stylesheet' type='text/css'>
+
+		<script type="text/javascript" src="js/modernizr.custom.js"></script>
+						
+				
+					<script type="text/javascript">
+					function toggle_visibility(id) {
+					   var e = document.getElementById(id);
+					   if(e.style.display == 'block')
+						  e.style.display = 'none';
+					   else
+						  e.style.display = 'block';
+					}
+					
+					
+					</script>
+						
+	  </head>
+	  <body>
+		<!-- Navigation
+		==========================================-->
+		<nav id="tf-menu" class="navbar navbar-default navbar-fixed-top">
+		  <div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			  <a class="navbar-brand" href="index.php">Colapex	</a>
+			  
+			</div>
+
+			<div id="popup-box" class="popup-position">
+				<div id="popup-wrapper">
+				<div id="popup-container">
+				<div id="top"><p>Log in To Colapex<a href="javascript:void(0)" onclick="toggle_visibility('popup-box');">X</a></p></div>
+				<div class="social_login">
+					<a href="#" class="social_box fb">
+					<div class="fb">
+						<span class="icon"><i class="fa fa-facebook"></i></span>
+						<span class="icon_title">Log in with Facebook</span>
+					</div>
+					
+					</a>
+					<h6>Recommanded, we'll never post anything in your timeline without your permission.</h6>	
+					<div class="google">
+					<a href="#" class="social_box google">
+						<span class="icon"><i class="fa fa-google-plus"></i></span>
+						<span class="icon_title">Log in with Google</span>
+					</a>
+					<h6>Use your Email Adress</h6>
+					</div>
+				</div>
+				
+				
+				<div id="loginbutton" style="float:left"><a href="javascript:void(0)" onclick="toggle_visibility('popup-box1');" >Log in</a>
+				</div>
+				<div id="signupbutton" style="float:left"><a href="javascript:void(0)" onclick="toggle_visibility('popup-box2');">Sign up</a>
+				</div>	
+				</div>
+				</div>
+			</div>
+			<!-- pop up login-->
+			<div id="popup-box1" class="popup-position">
+				<div id="	1">
+				<div id="popup-container1">
+				<div id="top"><p>Log in To Colapex<a href="javascript:void(0)" onclick="toggle_visibility('popup-box1');">X</a></p></div>
+				<h3> chutiyapa</h3>
+			</div>
+			</div>
+			</div>
+				
+			<!--pop up iit-->
+			<div id="popup-box-iit" class="popup-position-iit">
+				<div id="popup-wrapper-iit">
+				<div id="popup-container-iit">
+				<div class="main">
+					 <h6><a href="javascript:void(0)" onclick="toggle_visibility('popup-box-iit');">X</a></h6>
+					 <img  src="img/iitb.png" onmouseover="this.src='img/iitb_name.png'" onmouseout="this.src='img/iitb.png'" width="150" height="150"/>
+					 <img src="img/IIT_bhaneshwar.jpg" onmouseover="this.src='img/iitbhuv_name.png'" onmouseout="this.src='img/IIT_bhaneshwar.jpg'" width="150" height="150"/>
+						<img  src="img/iitd.png" onmouseover="this.src='img/iitd_name.png'" onmouseout="this.src='img/iitd.png'" width="150" height="150"/>	
+					 <img  src="img/gandhi.png" onmouseover="this.src='img/iitgandhi_name.png'" onmouseout="this.src='img/gandhi.png'" width="150" height="150"/>
+					 <img src="img/guwati.png" onmouseover="this.src='img/iitg_name.png'" onmouseout="this.src='img/guwati.png'" width="150" height="150"/>
+					 <img src="img/iith.png" onmouseover="this.src='img/iith_name.png'" onmouseout="this.src='img/iith.png'" width="150" height="150"/>
+					 <img src="img/indore.png" onmouseover="this.src='img/iiti_name.png'" onmouseout="this.src='img/indore.png'" width="150" height="150"/>
+					  <img src="img/jodhpur.png" onmouseover="this.src='img/iitj_name.png'" onmouseout="this.src='img/jodhpur.png'" width="150" height="150"/>
+					 <img src="img/iitroo.gif" onmouseover="this.src='img/iitr_name.png'" onmouseout="this.src='img/iitroo.gif'" width="150" height="150"/>
+					 <img src="img/ropar.jpg" onmouseover="this.src='img/iitropar_name.png'" onmouseout="this.src='img/ropar.jpg'" width="150" height="150"/>
+					 <img  src="img/iit_kanpur.png" onmouseover="this.src='img/iitk_name.png'" onmouseout="this.src='img/iit_kanpur.png'" width="150" height="150"/>
+					 <img src="img/iitkgp.jpg" onmouseover="this.src='img/iitkgp_name.png'" onmouseout="this.src='img/iitkgp.jpg'" width="150" height="150">
+					  <img src="img/madras.png" onmouseover="this.src='img/iitm_name.png'" onmouseout="this.src='img/madras.png'" width="150" height="150"/>
+					<img src="img/mandi.jpg" onmouseover="this.src='img/iitmandi_name.png'" onmouseout="this.src='img/mandi.jpg'" width="150" height="150"/>
+					<img src="img/patna.png" onmouseover="this.src='img/iitp_name.png'" onmouseout="this.src='img/patna.png'" width="150" height="150"/>
+					 <img src="img/bhu.png" onmouseover="this.src='img/iitbhu_name.png'" onmouseout="this.src='img/bhu.png'" width="150" height="150"/>
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+				</div>
+			</div>
+			</div>
+			</div>
+			<!-- pop up iims -->
+			<div id="popup-box-iim" class="popup-position-iim">
+				<div id="popup-wrapper-iim">
+				<div id="popup-container-iim">
+				<div class="main">
+					 <h6><a href="javascript:void(0)" onclick="toggle_visibility('popup-box-iim');">X</a></h6>
+					 <img id="myimage" onmouseover="changeImage()" onmouseout="changeImage()" src="img/iimc.gif" width="150" height="150"/>
+					 <img src="img/iima.png" width="150" height="150"/>
+						<img id="myimage1" onmouseover="changeImage1()" onmouseout="changeImage1()" src="img/iimb.png" width="150" height="150"/>	
+					 <img  src="img/iiml.png" width="150" height="150"/>
+					 <img src="img/iimk.png" width="150" height="150"/>
+					 <img src="img/iimi.jpg" width="150" height="150"/>
+					 <img src="img/iims.jpg" width="150" height="150"/>
+					  <img src="img/iimr.png" width="150" height="150"/>
+					 <img src="img/iimt.png" width="150" height="150"/>
+					 <img src="img/iimu.jpg" width="150" height="150"/>
+					 <img src="img/iimka.png" width="150" height="150"/>
+					 <img id="myimage2" onmouseover="changeImage2()" onmouseout="changeImage2()" src="img/iimn.png" width="150" height="150">
+					 
+	 
+				</div>
+			</div>
+			</div>
+			</div>
+			<!--pop up Dus -->
+			<div id="popup-box-du" class="popup-position-du">
+				<div id="popup-wrapper-du">
+				<div id="popup-container-du">
+				<div class="main1">
+					 <h6><a href="javascript:void(0)" onclick="toggle_visibility('popup-box-du');">X</a></h6>
+						
+					 </div>
+					
+					<input id="explore" type="text" name="searchtext" placeholder="Search colleges here...."/>
+					
+					 <div id="php">
+					 <?php
+						mysql_connect("localhost","root","");
+						mysql_select_db("my_colleges");
+						$qu="select *FROM du_colleges";
+						$result=mysql_query($qu);
+						$a=0;
+						$num=mysql_num_rows($result);
+						 if($num>0){
+							 
+					echo"<table>";
+						while ($row = mysql_fetch_assoc($result)){
+						   $colleges_name= $row['colleges_name'];
+						   $colleges_city= $row['colleges_city'];
+						   
+							echo " <h10><a href='####'>$colleges_name</a></h10></br>" ;
+							/*	
+								if($a%2	==0){
+									echo "<tr><td>";
+									echo " <h10><a href='####'>$colleges_name</a></h10>" ;  		
+									echo "</TD>";
+						}	
+						else{
+							echo "<td>";
+									echo " <h10><a href='####'>$colleges_name</a></h10>" ;  		
+									echo "</td>";
+						}
+					 */
+			$a++;
+			
+		   }
+		 
+	   }
+			
+		 echo"</table>";
+ ?>
+					 
+				</div>
+			</div>
+		</div>
+	</div>
+
+			
+			
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			
+			   <ul class="nav navbar-nav navbar-right">
+				<li ><a	 href="javascript:void(0);" onclick="toggle_visibility('popup-box');">Login</a></li>
+				<li><a href="#tf-services" >Log in with Facebook</a></li>
+				
+			  </ul>
+			 
+			</div><!-- /.navbar-collapse -->
+		  <!-- /.container-fluid -->
+		</nav>
+
+		<!-- Home Page
+		==========================================-->
+		<div id="tf-home" class="text-center">
+			<div class="overlay">
+				<div class="content">
+					<form id="searchbox" action='./index.php' method='get'>
+				<select id="filter" type='filter' name='filter'  placeholder="Enter Your location" >
+				<option value="Ahemdabad">Ahemdabad</option>
+				<option value="Banglore"> Banglore</option>
+				<option value="Bombay">Bombay</option>
+				<option value="Chandigarh">Chandigarh</option>
+				<option value="Delhi">Delhi</option>
+				<option value="Indore">Indore</option>
+				<option value="Kanpur">Kanpur</option>
+				<option value="Lucknow">Lucknow</option>
+				<option value="Other">Other</option>	
+				</select>
+                 <input id="search" type='text' name='search' placeholder="Enter colleges name or city">
+                 <input id="submit" type='submit'  value="Search">
+                </form>
+					
+				</div>
+			</div>
+		</div>
+		
+
+		
+		
+		
+		</br>
+	</body>	
+	 <div style="position:relative">
+		<div id = "leftblock" style="float:left;">
+		<table class='table'>
+		
+		<h7>Quick Searches</h7>
+		<br>
+		<tr>
+		<td style="cursor:pointer" onclick="toggle_visibility('popup-box-iit');"><a class="fa fa-angle-right" href="javascript:void(0);" >IITs</a></td></tr>	
+		<tr><td style="cursor:pointer" onclick="toggle_visibility('popup-box-iim');" ><a class="fa fa-angle-right" href="javascript:void(0);" >IIMs</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="document.location.href='aiims.html'" ><a class="fa fa-angle-right">AIIMS</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="document.location.href='bits.html'" ><a class="fa fa-angle-right">BITS</a></td></tr>
+		<!--<tr><td style="cursor:pointer" onclick="document.location.href='tre.html'" ><a class="fa fa-angle-right">Trending Colleges</a></td></tr>-->
+		<tr><td style="cursor:pointer" onclick="document.location.href='nit.html'" ><a class="fa fa-angle-right"> NITs</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="toggle_visibility('popup-box-du');" ><a class="fa fa-angle-right" href="javascript:void(0);" >DU Colleges</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="document.location.href='others.html'" ><a class="fa fa-angle-right"> See all colleges </a></td></tr>
+		</table>
+		<table class='table'>
+		<h7 >Trending Colleges</h7>
+		<tr>
+		<td style="cursor:pointer" onclick="document.location.href='iitd.html'"><a class="fa fa-angle-right">IIT Delhi</a></td></tr>	
+		<tr><td style="cursor:pointer" onclick="document.location.href='bitsp.html'" ><a class="fa fa-angle-right">BITS Pilani</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="document.location.href='iima.html'" ><a class="fa fa-angle-right">IIM Ahemdabad</a></td></tr>
+		<!--<tr><td style="cursor:pointer" onclick="document.location.href='tre.html'" ><a class="fa fa-angle-right">Trending Colleges</a></td></tr>-->
+		<tr><td style="cursor:pointer" onclick="document.location.href='nitw.html'" ><a class="fa fa-angle-right"> NIT Warangal</a></td></tr>
+		</table>
+		<table class='table'>
+		<h7 >Top Exams</h7>
+		<tr>
+		<td style="cursor:pointer" onclick="document.location.href='jee.html'"><a class="fa fa-angle-right">JEE Mains & Advanced </a></td></tr>	
+		<tr><td style="cursor:pointer" onclick="document.location.href='aiimsexam.html'" ><a class="fa fa-angle-right">AIIMS 2015</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="document.location.href='aipmt.html'" ><a class="fa fa-angle-right">AIPMT 2015</a></td></tr>
+		<!--<tr><td style="cursor:pointer" onclick="document.location.href='tre.html'" ><a class="fa fa-angle-right">Trending Colleges</a></td></tr>-->
+		<tr><td style="cursor:pointer" onclick="document.location.href='ias.html'" ><a class="fa fa-angle-right">IAS</a></td></tr>
+		</table>
+		</div>
+		<div id ="mysearch" style="float:left; ">
+	   <?php
+	  
+	  $search = $_GET['search'];	
+	  $terms = explode(" ",$search);
+	  $query = "SELECT * FROM colleges1 WHERE ";
+	  foreach($terms as $each){
+			  $i++;
+			  if($i ==1){
+				  $query .= "colleges_keywords LIKE '%$each%' ";
+			  }
+			  else
+			  {
+				   $query .= "OR colleges_keywords LIKE '%$each%' ";
+			  }
+			 
+		
+	  }
+	   //connection
+	   mysql_connect("localhost","root","");
+	   mysql_select_db("my_colleges");
+	   $a=0;
+	   $query = mysql_query($query);
+	   $numrows = mysql_num_rows($query);
+	   
+	   if($numrows>0){
+		   echo"<table>";
+		   echo"<h7 >City Colleges</h7></br>";
+		   
+		 echo "<table class='table  table-striped >";
+		 
+			
+				 $names=array();
+			 $city=array();
+			 $link=array();
+		   while ($row = mysql_fetch_assoc($query)){
+			   $colleges_name= $row['colleges_name'];
+			   $colleges_city= $row['colleges_city'];
+			   $colleges_branch= $row['colleges_branch'];
+				$colleges_link= $row['colleges_link'];
+			   $colleges_key=$row['colleges_keywords'];
+			   $colleges_contact=$row['colleges_contact'];
+			   $colleges_mail=$row['college_email'];
+			   $names[]=$colleges_name;
+			   $city[]=	$colleges_city;
+			   $link[]=$colleges_link;
+			
+			{
+					echo "<tr style='padding-bottom:16px' > <td  >";
+						echo " <h3><a href='####'>$colleges_name</a></h3>" ;  		
+						echo   "<h8> $colleges_city</h8><br>";
+						echo   "<h8><img src='img/phone.png' width='15px' height='15px'> $colleges_contact</h8><br>";
+						echo   "<h8><img src='img/mail.png' width='15px' height='15px'>  	$colleges_mail</h8><br>";	
+						echo "</TD>";
+					echo"<tr><td><p></p>
+					<p></p><p></p>
+					</td></tr>";	
+			}
+			
+			
+		/* if($a%3==0){
+						echo "<tr><td>";
+						echo " <h3><a href='####'>$colleges_name</a></h3>" ;  		
+						echo   "<p>$colleges_city</p>";
+						echo   "<p>$colleges_contact</p>";
+						echo   "<h7>Email:$colleges_mail</h7>";
+						echo "</TD>";
+			}	
+			else{
+				echo "<td>";
+						echo " <h3><a href='####'>$colleges_name</a></h3>" ;  		
+						echo   "<p>$colleges_city</p>";
+						echo   "<p>$colleges_contact</p>";
+						echo   "<h7>Email:$colleges_mail</h7>";
+						echo "</td>";
+			}
+			
+	*/   
+
+			$a++;
+			
+		   }
+		 
+	   }
+	   
+	   else{
+		   echo "No query found";
+	   }
+	   
+	   
+	   //disconnect
+	   mysql_close();
+	 ?>
+	</table>
+	 </div>  
+	<div style="position:relative">
+		<div id = "rightbox" style="float:left;">
+		<table class='table'>
+		
+		<h7 >Recent Events</h7>
+		<tr>
+		<td style="cursor:pointer" onclick="document.location.href='iit.html'"><a class="fa fa-angle-right">Search by Location</a></td></tr>	
+		<tr><td style="cursor:pointer" onclick="document.location.href='iim.html'" ><a class="fa fa-angle-right">Search by college</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="document.location.href='aiims.html'" ><a class="fa fa-angle-right">Search by stream</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="document.location.href='bits.html'" ><a class="fa fa-angle-right">all </a></td></tr>
+		<!--<tr><td style="cursor:pointer" onclick="document.location.href='tre.html'" ><a class="fa fa-angle-right">Trending Colleges</a></td></tr>-->
+		<tr><td style="cursor:pointer" onclick="document.location.href='nit.html'" ><a class="fa fa-angle-right"> NITs</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="document.location.href='du.html'" ><a class="fa fa-angle-right">DU Colleges</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="document.location.href='others.html'" ><a class="fa fa-angle-right"> See all colleges </a></td></tr>
+		</table>
+		<table class='table'>
+		<h7 >Trending Colleges</h7>
+		<tr>
+		<td style="cursor:pointer" onclick="document.location.href='iitd.html'"><a class="fa fa-angle-right">IIT Delhi</a></td></tr>	
+		<tr><td style="cursor:pointer" onclick="document.location.href='bitsp.html'" ><a class="fa fa-angle-right">BITS Pilani</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="document.location.href='iima.html'" ><a class="fa fa-angle-right">IIM Ahemdabad</a></td></tr>
+		<!--<tr><td style="cursor:pointer" onclick="document.location.href='tre.html'" ><a class="fa fa-angle-right">Trending Colleges</a></td></tr>-->
+		<tr><td style="cursor:pointer" onclick="document.location.href='nitw.html'" ><a class="fa fa-angle-right"> NIT Warangal</a></td></tr>
+		</table>
+		<table class='table'>
+		<h7 >Top Exams</h7>
+		<tr>
+		<td style="cursor:pointer" onclick="document.location.href='jee.html'"><a class="fa fa-angle-right">JEE Mains & Advanced </a></td></tr>	
+		<tr><td style="cursor:pointer" onclick="document.location.href='aiimsexam.html'" ><a class="fa fa-angle-right">AIIMS 2015</a></td></tr>
+		<tr><td style="cursor:pointer" onclick="document.location.href='aipmt.html'" ><a class="fa fa-angle-right">AIPMT 2015</a></td></tr>
+		<!--<tr><td style="cursor:pointer" onclick="document.location.href='tre.html'" ><a class="fa fa-angle-right">Trending Colleges</a></td></tr>-->
+		<tr><td style="cursor:pointer" onclick="document.location.href='ias.html'" ><a class="fa fa-angle-right">IAS</a></td></tr>
+		</table>
+		</div>
+	</div>
+
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.1.11.1.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script type="text/javascript" src="js/bootstrap.js"></script>
+		<script type="text/javascript" src="js/SmoothScroll.js"></script>
+		<script type="text/javascript" src="js/jquery.isotope.js"></script>
+
+		<script src="js/owl.carousel.js"></script>
+
+		<!-- Javascripts
+		================================================== -->
+		<script type="text/javascript" src="js/main.js"></script>
+
+	</html>
